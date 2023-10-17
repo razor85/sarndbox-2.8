2e5afdf15f19d7df740162af8a167e9ad7ca8a4d
Romulo Leitão <abra185@gmail.com>
2023-10-17 15:01:48 -0300

    compile, depth saves normally

--- Config.h
+++ Config.h
@@ -22,8 +22,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #ifndef CONFIG_INCLUDED
 #define CONFIG_INCLUDED
 
-#define CONFIG_CONFIGDIR "/usr/local/etc/SARndbox-2.4"
-#define CONFIG_SHADERDIR "/usr/local/share/SARndbox-2.4/Shaders"
+#define CONFIG_CONFIGDIR "/home/arsandbox/src/SARndbox-2.8/etc/SARndbox-2.8"
+#define CONFIG_SHADERDIR "/home/arsandbox/src/SARndbox-2.8/share/SARndbox-2.8/Shaders"
 
 #define CONFIG_DEFAULTCONFIGFILENAME "SARndbox.cfg"
 #define CONFIG_DEFAULTBOXLAYOUTFILENAME "BoxLayout.txt"
